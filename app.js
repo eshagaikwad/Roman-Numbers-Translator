df828f03bf8fef6e76f7e7c88c7bef10a3c2ddc8
eshagaikwad <74804285+eshagaikwad@users.noreply.github.com>
2021-07-17 16:59:51 +0530

    error sloved

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 var serverURL = "https://api.funtranslations.com/translate/roman-numerals.json"
 
@@ -18,9 +18,9 @@ function errorHandler(error) {
 
 
 function clickHandler() {
-    var inputText = txtInput.value; 
+    var inputText = txtInput.value; // taking input
 
-    
+    // calling server for processing
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
