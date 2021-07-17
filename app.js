8797782466c4e63b43db69cdb1e4d2f2931f7d07
eshagaikwad <74804285+eshagaikwad@users.noreply.github.com>
2021-07-17 13:40:57 +0530

    all things updated

--- app.js
+++ app.js
@@ -2,34 +2,32 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL="https://api.funtranslations.com/translate/minion.json";
 
- function getTranslationURL(input) {
-     return serverURL + "?" + "text= " + input
- }
 
- function errorHandler(error) {
-     console.log("error ocurred",error);
-    alert("something went wrong with the server! try after some time ")
- }
+var serverURL = "https://api.funtranslations.com/translate/roman-numerals.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
 
 function clickHandler() {
-    var inputText = txtInput.value;
+    var inputText = txtInput.value; 
 
+    
     fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json=> { 
-        var translatedText = json.contents.translated;
-      outputDiv.innerText = translatedText;  
-    })
-    .catch(errorHandler)
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
-
-
-
-
-
-
-   
